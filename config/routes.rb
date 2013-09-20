c62cda1cc1b3b609db450c8efa9e2062d5c5aee3
Tim Tyrrell <tim@tyrrell.io>
2013-09-19 21:11:43 -0500

    Add create route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 OutsideInDev::Application.routes.draw do
-  resources :posts, only: :new
+  resources :posts, :only => [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
