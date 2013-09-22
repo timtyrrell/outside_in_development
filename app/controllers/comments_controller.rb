bc78f5d7feaebebe97243527e4b483cd489f4cc5
Tim Tyrrell <tim@tyrrell.io>
2013-09-21 22:19:07 -0500

    Add comment strong parameters

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,18 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find params[:post_id]
-    @comment = @post.comments.build params[:comment]
+    @comment = @post.comments.build comment_params
 
     if @comment.save
       redirect_to post_path(@post), :notice => "New comment created!"
     else
       render :new
     end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:retort)
   end
 end
