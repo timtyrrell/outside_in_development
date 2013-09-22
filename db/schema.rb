f69d60683bf2fa5ea24ada486a27439946b3dd26
Tim Tyrrell <tim@tyrrell.io>
2013-09-21 22:01:30 -0500

    Add Comment model as a Post association

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130920020048) do
+ActiveRecord::Schema.define(version: 20130921214202) do
+
+  create_table "comments", force: true do |t|
+    t.text     "retort"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
   create_table "posts", force: true do |t|
     t.string   "title"
