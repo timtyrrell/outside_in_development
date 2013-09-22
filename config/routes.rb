4c0a52b352b18ba76540f0800896eedecaf4b596
Tim Tyrrell <tim@tyrrell.io>
2013-09-21 22:09:31 -0500

    Add comments route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 OutsideInDev::Application.routes.draw do
-  resources :posts, :only => [:new, :create, :show, :index]
+  resources :posts, :only => [:new, :create, :show, :index] do
+    resources :comments, :only => [:create]
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
