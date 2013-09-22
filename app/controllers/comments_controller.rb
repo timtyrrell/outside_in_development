87bec7304ae84ccaccaa38ee8c4812061d261e75
Tim Tyrrell <tim@tyrrell.io>
2013-09-21 22:17:06 -0500

    Add comments#create functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,12 @@
 class CommentsController < ApplicationController
   def create
+    @post = Post.find params[:post_id]
+    @comment = @post.comments.build params[:comment]
 
+    if @comment.save
+      redirect_to post_path(@post), :notice => "New comment created!"
+    else
+      render :new
+    end
   end
 end
